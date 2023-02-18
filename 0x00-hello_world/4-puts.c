8fcc5ff1c31d6014914d0b0ed700fa99adf65fc6
m-gsani <martin.n.gasani@gmail.com>
2023-02-18 07:38:36 +0000

    Updated task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
-/* printing program */
+/* function main prints a statment */
 #include <stdio.h>
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle\n");
 	char ch;
+
 	scanf("%c", &ch);
-	return 0;
+	return (0);
 }
