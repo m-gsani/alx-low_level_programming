436416008854ad6ea390729cdae2a7c8e73014c6
m-gsani <martin.n.gasani@gmail.com>
2023-02-18 07:44:18 +0000

    Updated 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,12 @@
 /* function main prints a statment */
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * print programming is like building a multilingual puzzle
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle\n");
