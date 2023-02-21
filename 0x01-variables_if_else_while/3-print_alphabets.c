112b8a9c0a2d2121efec9f5d8ce0a461518977f9
m-gsani <martin.n.gasani@gmail.com>
2023-02-21 04:45:50 +0000

    Updated task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,12 +10,12 @@ int main(void)
 	char lower_case = 'a';
 	char upper_case = 'A';
 
-	while (lower_case < 'z')
+	while (lower_case <= 'z')
 	{
 		putchar(lower_case);
 		lower_case++;
 	}
-	while (upper_case < 'Z')
+	while (upper_case <= 'Z')
 	{
 		putchar(upper_case);
 		upper_case++;
