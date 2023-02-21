72980a0f67495072be83497f97801c11c110ce48
m-gsani <martin.n.gasani@gmail.com>
2023-02-21 05:02:42 +0000

    Updated task 5

---
+++ 0x01-variables_if_else_while/5-print_numbres.c
@@ -0,0 +1,21 @@
+#include <stido.h>
+/**
+ * main - Entry point
+ * Description: 'Prints all singel digit base 10 numbers'
+ * Return: Alwyas 0 (success)
+ *
+ */
+
+int main(void)
+{
+	int a = 0;
+
+	if (a <= 9)
+	{
+		printf(a);
+		a++;
+	}
+	printf('\n');
+
+	return (0);
+}
