da72a13b3c6e01372c5b01dab4d8da4561b10f19
m-gsani <martin.n.gasani@gmail.com>
2023-02-21 04:15:30 +0000

    Updated task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,7 +11,7 @@ int main(void)
 {
 	int n;
 
-	strand(time(0));
+	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
@@ -25,4 +25,5 @@ int main(void)
 	{
 		printf("%d is negative\n", n);
 	}
+
 	return (0);
