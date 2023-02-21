2f1f9836563f2b28e6b2507a98e5fcb78c9b33c4
m-gsani <martin.n.gasani@gmail.com>
2023-02-21 04:04:13 +0000

    Updated task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,11 +1,17 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
+/**
+ * main - Entry point
+ *
+ * Description: 'the program prints out the alphabet'
+ *
+ * Return: Always 0 (Success)
+ */
 
-/* betty style doc for function main goes there */
 int main(void)
 {
+
 	int n;
 	int last_digit;
 
