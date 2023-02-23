4ad6c93749046d4b212fc391a93f9a324576cd82
m-gsani <martin.n.gasani@gmail.com>
2023-02-23 09:28:13 +0000

    main

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,7 +3,7 @@
 
 int print_putchar(void);
 void print_alphabet(void);
-void print_alphabet_10x(void);
+void print_alphabet_x10(void);
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
