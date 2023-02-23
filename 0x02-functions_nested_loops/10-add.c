25954dd4ea2e671cc8bb22a586bb94a12dcf201e
m-gsani <martin.n.gasani@gmail.com>
2023-02-23 09:47:36 +0000

    added task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - adds two values a and b
+ * @a : first number
+ * @b : second number
+ * @c : store for the addition
+ * Return : c if successful
+ */
+
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
