069d7b48686419a001763906ca0e2f7658998d87
m-gsani <martin.n.gasani@gmail.com>
2023-02-25 07:00:52 +0000

    Late submission

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* main - tests function whether gives the correct output when given a case of 0
+* Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
+
