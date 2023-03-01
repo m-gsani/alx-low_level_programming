c05ffeb13a386c61b7f0664a34beb9e147c32451
m-gsani <martin.n.gasani@gmail.com>
2023-03-01 05:45:01 +0000

    Updated task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,9 +6,13 @@
 void print_rev(char *s)
 {
 	int i;
-	int end;
+	int end = 0;
 
-	end = _strlen(s);
+        while (s[end] != '\0')
+        {
+                end++;
+        }
+
 	for (i = end - 1; i >= 0; i--)
 	{
 		_putchar(s[i]);
