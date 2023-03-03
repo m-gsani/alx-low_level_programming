2b5997e859bd5cb5d8e83c57b1e52088349b6606
m-gsani <martin.n.gasani@gmail.com>
2023-03-03 13:05:28 +0000

    Updated task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @s: string to encode
+ *
+ * Return: encoded string
+ */
+char *leet(char *s)
+{
+  int i, j;
+  char letters[] = "aAeEoOtTlL";
+  char numbers[] = "4433007711";
+
+  for (i = 0; s[i] != '\0'; i++)
+  {
+    for (j = 0; letters[j] != '\0'; j++)
+    {
+      if (s[i] == letters[j])
+        s[i] = numbers[j];
+    }
+  }
+  return (s);
+}
