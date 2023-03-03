4a4d821a0c8e5c26e61e1a13f3267e13d569f6ac
m-gsani <martin.n.gasani@gmail.com>
2023-03-03 12:57:35 +0000

    Late submission

---
+++ 0x01-variables_if_else_while/100-times_table.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints the n times table, starting with 0
+ * @n: the number of times
+ *
+ * Return: void
+ */
+void print_times_table(int n)
+{
+	int i, j, k;
+
+	if (n < 0 || n > 15)
+		return;
+
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			k = i * j;
+			if (j == 0)
+				_putchar('0' + k);
+			else if (k < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + k);
+			}
+			else if (k < 100)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + k / 10);
+				_putchar('0' + k % 10);
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar('0' + k / 100);
+				_putchar('0' + (k / 10) % 10);
+				_putchar('0' + k % 10);
+			}
+		}
+		_putchar('\n');
+	}
+}
+
